b434fe55e3f6ea8b1686e347ae673f1ae152a072
Mario <92472105+OneTuskedMario@users.noreply.github.com>
2022-06-09 04:18:21 +0200

    Register Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,32 +4,61 @@ import Image from 'next/image';
 import { useState } from 'react';
 import logo from '../components/logo.svg';
 import { supabase } from '../utils/supabaseClient';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { useForm } from 'react-hook-form';
 const Home: NextPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const signInWithEmail = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    const { error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-    if (error) throw error;
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+
+  const formSchema = Yup.object().shape({
+    email: Yup.string().required(),
+    password: Yup.string().min(8, 'Password must be at least 8 characters').required(),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password')], 'Passwords do not match')
+      .min(8, 'Password must be at least 8 characters')
+      .required(),
+    name: Yup.string().required(),
+    surname: Yup.string().required(),
+  });
+  const formOptions = { resolver: yupResolver(formSchema) };
+  const { register, handleSubmit, formState } = useForm(formOptions);
+  const { errors } = formState;
+  const onSubmit = async () => {
+    const data = await supabase.auth.signUp(
+      {
+        email: email,
+        password: password,
+      },
+      {
+        data: {
+          first_name: name,
+          last_name: surname,
+        },
+      }
+    );
+    console.log(data);
+    if (data.error) throw data.error;
   };
 
   return (
     <div className="flex w-full flex-wrap">
       <div className="flex w-full flex-col md:w-1/2">
-        <div className="flex justify-center pt-12 md:-mb-24 md:justify-start md:pl-12">
-          <a href="#" className="bg-black p-4 text-xl font-bold text-white">
-            Logo
+        <div className="flex justify-center pt-6 md:-mb-6 md:justify-start md:pl-8">
+          <a href="#" className="bg-black p-4 text-xl font-extrabold text-white">
+            Learn.io
           </a>
         </div>
 
         <div className="my-auto flex flex-col justify-center px-8 pt-8 md:justify-start md:px-24 md:pt-0 lg:px-32">
-          <p className="text-center text-3xl">Weclome To Learn.io</p>
-          <form className="flex flex-col pt-3 md:pt-8">
+          <p className="text-center text-3xl font-semibold">Welcome to our platform.</p>
+          <form className="flex flex-col pt-3 md:pt-3" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col pt-4">
-              <label htmlFor="email" className="text-lg">
+              <label htmlFor="email" className="ml-2 text-lg">
                 Email
               </label>
               <input
@@ -37,13 +66,14 @@ const Home: NextPage = () => {
                 id="email"
                 placeholder="your@email.com"
                 value={email}
+                {...register('email')}
                 onInput={(e) => setEmail((e.target as HTMLInputElement).value)}
                 className="focus:shadow-outline mt-1 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
               />
             </div>
 
             <div className="flex flex-col pt-4">
-              <label htmlFor="password" className="text-lg">
+              <label htmlFor="password" className="ml-2 text-lg">
                 Password
               </label>
               <input
@@ -51,30 +81,76 @@ const Home: NextPage = () => {
                 id="password"
                 placeholder="Password"
                 value={password}
+                {...register('password')}
                 onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
+                className="focus:shadow-outline mt-1 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+              <div className="text-error text-center">{errors.confirmPassword?.message}</div>
+            </div>
+
+            <div className="flex flex-col pt-4">
+              <label htmlFor="password" className="ml-2 text-lg">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Repeat Password"
+                value={confirmPassword}
+                {...register('confirmPassword')}
+                onInput={(e) => setConfirmPassword((e.target as HTMLInputElement).value)}
+                className="focus:shadow-outline mt-1 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+              <div className="text-error text-center">{errors.confirmPassword?.message}</div>
+            </div>
+
+            <div className="flex flex-col pt-4">
+              <label htmlFor="password" className="ml-2 text-lg">
+                First Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Name"
+                value={name}
+                {...register('name')}
+                onInput={(e) => setName((e.target as HTMLInputElement).value)}
+                className="focus:shadow-outline mt-1 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+            </div>
+            <div className="flex flex-col pt-4">
+              <label htmlFor="password" className="ml-2 text-lg">
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="surname"
+                placeholder="Surname"
+                value={surname}
+                {...register('surname')}
+                onInput={(e) => setSurname((e.target as HTMLInputElement).value)}
                 className="focus:shadow-outline mt-1 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
               />
             </div>
 
             <button
               type="submit"
-              onClick={signInWithEmail}
               className="mt-8 bg-black p-2 text-lg font-bold text-white hover:bg-gray-700"
             >
-              Login
+              Register
             </button>
           </form>
-          <div className="pt-12 pb-12 text-center">
+          <div className="pt-8 text-center">
             <p>
               <a href="register.html" className="font-semibold underline">
-                Register here.
+                Are you a teacher?
               </a>
             </p>
           </div>
         </div>
       </div>
 
-      <div className="flex h-screen w-1/2 shadow-2xl">
+      <div className="max-w-1/2 flex sm:h-screen sm:w-1/2 sm:shadow-2xl">
         <div className="inset-0 my-auto">
           <Image width="1000px" className="mt-10 h-screen w-full" src={logo} alt="test" />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      custom: ['Rubik', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
